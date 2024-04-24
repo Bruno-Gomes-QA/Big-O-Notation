7d60c50bc30f0e9b254829e4c47e2a5844774aaa
Bruno-Gomes-QA <68879612+Bruno-Gomes-QA@users.noreply.github.com>
2024-04-24 22:35:26 +0000

    O(n)

--- O-logn-.py
+++ O-logn-.py
@@ -15,5 +15,5 @@ def binary_search(arr, target):
             
     return -1  
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+arr = list(range(1,100))
 print(binary_search(arr, 20))
---
+++ O-n.py
@@ -0,0 +1,11 @@
+# O(n): Implement an algorithm that traverses all elements of a list and returns the index of the searched element, if present.
+
+def search_element(arr, target):
+    for i, e in enumerate(arr):
+        if e == target:
+            return i
+
+    return -1
+
+arr = list(range(1,100))
+print(search_element(arr, 100))
